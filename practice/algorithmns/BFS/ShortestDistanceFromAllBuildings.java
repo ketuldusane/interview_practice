452a06067367afc7be802ffe03053beabc12bb62
ketuldusane <ketul.dusane@gmail.com>
2020-02-29 19:56:17 -0500

    "Solutions"

---
+++ practice/algorithmns/BFS/ShortestDistanceFromAllBuildings.java
@@ -0,0 +1,112 @@
+package algorithmns.BFS;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * Shortest Distance From All Buildings
+ * <p>
+ * You want to build a house on an empty land which reaches all buildings in the shortest amount of distance. You can
+ * only move up, down, left and right. You are given a 2D grid of values 0, 1 or 2, where:
+ * <p>
+ * Each 0 marks an empty land which you can pass by freely.
+ * Each 1 marks a building which you cannot pass through.
+ * Each 2 marks an obstacle which you cannot pass through.
+ * Example:
+ * <p>
+ * Input: [[1,0,2,0,1],[0,0,0,0,0],[0,0,1,0,0]]
+ * <p>
+ * 1 - 0 - 2 - 0 - 1
+ * |   |   |   |   |
+ * 0 - 0 - 0 - 0 - 0
+ * |   |   |   |   |
+ * 0 - 0 - 1 - 0 - 0
+ * <p>
+ * Output: 7
+ * <p>
+ * Explanation: Given three buildings at (0,0), (0,4), (2,2), and an obstacle at (0,2),
+ * the point (1,2) is an ideal empty land to build a house, as the total
+ * travel distance of 3+3+1=7 is minimal. So return 7.
+ * Note:
+ * There will be at least one building. If it is not possible to build such house according to above rules, return -1.
+ */
+
+public class ShortestDistanceFromAllBuildings {
+  // Apply standard BFS
+  // Instead of searching from '0' and trying to reach '1',
+  // search from '1' and fill up a 2D array with distances to 0
+  // The '0' index with min distance in the 2D array is the answer
+
+  public int shortestDistance(int[][] grid) {
+    if (grid == null || grid[0].length == 0) {
+      return -1;
+    }
+
+    int[][] distances = new int[grid.length][grid[0].length];
+    int[][] reach = new int[grid.length][grid[0].length];
+    int numOfHouses = 0;
+    int distance = Integer.MAX_VALUE;
+
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == 1) {
+          bfs(grid, i, j, distances, reach);
+          numOfHouses++;
+        }
+      }
+    }
+
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == 0 && reach[i][j] == numOfHouses) {
+          distance = Math.min(distances[i][j], distance);
+        }
+      }
+    }
+
+    return distance == Integer.MAX_VALUE ? -1 : distance;
+  }
+
+  private void bfs(int[][] grid, int i, int j, int[][] distances, int[][] reach) {
+    int distance = 1;
+    Deque<int[]> queue = new ArrayDeque<>();
+    boolean[][] visited = new boolean[grid.length][grid[0].length];
+    queue.offer(new int[]{i, j});
+    visited[i][j] = true;
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      for (int k = 0; k < size; k++) {
+        int[] current = queue.poll();
+        for (int[] neighbor : getNeighbors(grid, current)) {
+          int x = neighbor[0];
+          int y = neighbor[1];
+          if (!visited[x][y]) {
+            reach[x][y] += 1;
+            visited[x][y] = true;
+            distances[x][y] += distance;
+            queue.offer(neighbor);
+          }
+        }
+      }
+      distance++;
+    }
+  }
+
+  private List<int[]> getNeighbors(int[][] grid, int[] current) {
+    int[][] directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    List<int[]> neighbors = new ArrayList<>();
+
+    for (int[] direction : directions) {
+      int i = current[0] + direction[0];
+      int j = current[1] + direction[1];
+      if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 0) {
+        neighbors.add(new int[]{i, j});
+      }
+    }
+
+    return neighbors;
+  }
+}
---
+++ practice/design/OrganizationStructure.java
@@ -0,0 +1,130 @@
+package design;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class OrganizationStructure {
+  private Map<String, Employee> employeeMap = new HashMap<>();
+  private Map<Integer, Employee> rootManagers = new TreeMap<>();
+  private int time = 0;
+
+  public static void main(String[] args) {
+    OrganizationStructure o = new OrganizationStructure();
+    o.add("1", "S", "-1");
+    o.add("5", "D", "1");
+    o.add("8", "L", "-1");
+    o.add("9", "L1", "8");
+    o.add("6", "F", "1");
+    o.remove("8");
+    o.print();
+  }
+
+  public void add(String id, String name, String managerId) {
+    Employee employee = new Employee(id, name, managerId, time);
+    employeeMap.put(id, employee);
+    if (managerId.equals("-1")) {
+      rootManagers.put(time, employee);
+    }
+    time++;
+  }
+
+  public void remove(String id) {
+    Employee employee = employeeMap.get(id);
+    employeeMap.remove(id);
+    for (Employee e : employeeMap.values()) {
+      if (e.managerId.equals(id)) {
+        e.managerId = employee.managerId;
+        if (e.managerId.equals("-1")) {
+          rootManagers.put(e.time, e);
+        }
+      }
+    }
+    for (int t : rootManagers.keySet()) {
+      if (rootManagers.get(t).id.equals(id)) {
+        rootManagers.remove(t);
+        break;
+      }
+    }
+  }
+
+  public void update(String employeeId, String newManagerId) {
+    employeeMap.get(employeeId).managerId = newManagerId;
+    if (newManagerId.equals("-1")) {
+      rootManagers.putIfAbsent(employeeMap.get(employeeId).time, employeeMap.get(employeeId));
+    }
+  }
+
+  public int count(String employeeId) {
+    Map<String, Set<Employee>> map = getManagerMap();
+    return searchAndCount(map, new HashSet<>(), employeeId);
+  }
+
+  public void print() {
+    Map<String, Set<Employee>> map = getManagerMap();
+    for (Employee employee : rootManagers.values()) {
+      print(employee, map, "");
+    }
+  }
+
+  private void print(Employee employee, Map<String, Set<Employee>> map, String spaces) {
+    System.out.println(spaces + employee);
+    if (map.containsKey(employee.id)) {
+      List<Employee> employees = new ArrayList<>(map.get(employee.id));
+      Collections.sort(employees);
+      for (Employee e : employees) {
+        print(e, map, spaces + "  ");
+      }
+    }
+  }
+
+  private int searchAndCount(Map<String, Set<Employee>> map, Set<String> visited, String employeeId) {
+    int ans = 0;
+    for (Employee employee : map.getOrDefault(employeeId, new HashSet<>())) {
+      if (!visited.contains(employee.id)) {
+        visited.add(employee.id);
+        ans = ans + 1 + searchAndCount(map, visited, employee.id);
+      }
+    }
+    return ans;
+  }
+
+  private Map<String, Set<Employee>> getManagerMap() {
+    Map<String, Set<Employee>> map = new HashMap<>();
+    for (String id : employeeMap.keySet()) {
+      Employee employee = employeeMap.get(id);
+      map.putIfAbsent(employee.managerId, new HashSet<>());
+      map.get(employee.managerId).add(employee);
+    }
+    return map;
+  }
+
+  private static class Employee implements Comparable<Employee> {
+    String id;
+    String name;
+    String managerId;
+    int time;
+
+    public Employee(String id, String name, String managerId, int time) {
+      this.id = id;
+      this.name = name;
+      this.managerId = managerId;
+      this.time = time;
+    }
+
+    @Override
+    public String toString() {
+      return name + " [" + id + "]";
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+      return this.time - o.time;
+    }
+  }
+}
