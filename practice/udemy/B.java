e35637202819928bb192b65d19c0adfa4fa7d486
ketul <ketul.dusane@gmail.com>
2022-03-26 19:11:21 -0700

    test

--- practice/udemy/B.java
+++ practice/udemy/B.java
@@ -10,6 +10,7 @@ public class B {
     public static void generateFiles(String input_file_name) throws Exception {
         // product name : <total orders, qty>
         Map<String, ProductQtyDetails> qtyDetails = new HashMap<>();
+        Map<String, Map<String, Integer>> brandDetails = new HashMap<>();
 
         int n = 0;
         Scanner scanner = new Scanner(new File(""));
@@ -21,13 +22,22 @@ public class B {
                 int qty = Integer.parseInt(data[3]);
                 String brand = data[4];
 
-                qtyDetails.putIfAbsent(name, new ProductQtyDetails())
+                qtyDetails.putIfAbsent(name, new ProductQtyDetails(0, 0));
+                ProductQtyDetails p = qtyDetails.get(name);
+                p.orders++;
+                p.qty += qty;
+                qtyDetails.put(name, p);
+
+                Map<String, Integer> brands = brandDetails.getOrDefault(name, new HashMap<>());
+                brands.putIfAbsent(brand, 0);
+                brands.put(brand, brands.get(brand) + 1);
             }
+            n++;
         }
         scanner.close();
     }
 
-    class ProductQtyDetails {
+    static class ProductQtyDetails {
         int orders;
         int qty;
 
